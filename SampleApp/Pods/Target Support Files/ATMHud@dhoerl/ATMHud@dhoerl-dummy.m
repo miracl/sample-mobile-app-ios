efdbb921208185327ffcd7f5d56b2248d08a880b
avlaev <aleksandar.vlaev@gmail.com>
2019-04-16 12:16:29 +0300

    Rename WebsiteLoginSample target in Xcode

--- SampleApp/Pods/Target Support Files/ATMHud@dhoerl/ATMHud@dhoerl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ATMHud_dhoerl : NSObject
-@end
-@implementation PodsDummy_ATMHud_dhoerl
-@end
--- SampleApp/Pods/Target Support Files/Pods-WhiteLabels-Milagro/Pods-WhiteLabels-Milagro-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WhiteLabels_Milagro : NSObject
-@end
-@implementation PodsDummy_Pods_WhiteLabels_Milagro
-@end
